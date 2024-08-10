35f887a25fa9b0a194d43db0577a80279af71cae
Jash Shah <105346233+jash0803@users.noreply.github.com>
2024-08-10 20:08:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from PyPDF2 import PdfReader
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
-#import openpyxl
+import openpyxl
 from docx.api import Document
 
 
